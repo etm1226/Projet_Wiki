33eed3aaaf7f5338eee80b42c413960269aeff3b
A692022 <46968207+A692022@users.noreply.github.com>
2019-03-21 14:18:34 +0100

    Create install_php_fpm.sh

---
+++ mediawiki/install/script/install_php_fpm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+echo "installation de php-7"
+ yum -y install ImageMagick php70w-fpm php70w-intl php70w-xml php70w-curl \
+ php70w-gd php70w-mbstring php70w-mysql php70w-cli php70w-pear php70w-pecl-apcu
